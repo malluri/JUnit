9635587772c16611fa09454def8249743a835e8e
malluri <manoj.alluri00@gmail.com>
2019-01-25 11:10:39 +0530

    mockito

---
+++ mockito/src/main/java/com/selenium/testing/mockito/App.java
@@ -0,0 +1,13 @@
+package com.selenium.testing.mockito;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
